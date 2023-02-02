8c66d4747ce81e42d483f183cb5d03b2c77ffdaa
asifikbal09 <asif.ikbal662@gmail.com>
2023-02-02 15:39:29 +0600

    js-1

---
+++ moneyBack.js
@@ -0,0 +1,8 @@
+let moneyGiven = 1000;
+let applePrice = 300;
+let orangePrice = 400;
+
+let totalPrice = applePrice + orangePrice;
+
+let giveBack = moneyGiven - totalPrice;
+console.log(giveBack);
