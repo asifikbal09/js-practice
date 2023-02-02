d43b86146f8c452809b7eb2e3256266d3634ca6e
asifikbal09 <asif.ikbal662@gmail.com>
2023-02-02 16:06:19 +0600

    js 2

---
+++ average.js
@@ -0,0 +1,10 @@
+let math = 75.25;
+let biology = 65;
+let physics = 80;
+let chemistry = 35.45;
+let bangla = 99.50;
+
+let totalMarks = math + biology + physics + chemistry + bangla;
+
+let average = totalMarks / 5;
+console.log('Average Mark = ', average.toFixed(2));
